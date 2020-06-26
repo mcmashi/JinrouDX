a8f063e2c579b49e9747664e9ad240d22a6602e2
mashi <advancedcreator7@gmail.com>
2020-06-26 19:18:37 +0900

    2020/06/26進捗
    ・追加
    画像データ：
    音データ：
    スクリプト：セレクト、ゲームマネージャー
    ・概要
    画面でプレイ人数、役職数を決定しゲームを開始できる。

---
+++ Assets/Script/GManager.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GManager : MonoBehaviour {
+    #region ステート
+    enum State
+    {
+        FirstCheck = 1,
+
+        Night,
+
+        Morning,
+
+        DayCheck,
+
+        End
+
+    }
+    #endregion
+
+    #region ステート
+    enum Chara
+    {
+        Villager = 1,
+
+        Teller,
+
+        Medium,
+
+        Werewolf,
+
+        Madman
+    }
+    #endregion
+
+
+
+    #region オブジェクト
+
+    public GameObject Selector;
+
+    private Selector selectorscript;
+
+    #endregion
+
+    #region 変数
+
+    State game;
+
+
+    private int[] chara = new int[6];
+
+
+
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+        selectorscript = Selector.GetComponent<Selector>();
+
+        game = State.FirstCheck;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!selectorscript.GameState()) return;
+
+        switch (game)
+        {
+            case State.FirstCheck:
+                firstcheck();
+
+                break;
+
+            case State.Night:
+                night();
+
+                break;
+
+            case State.Morning:
+                morning();
+
+                break;
+
+            case State.DayCheck:
+                daycheck();
+
+                break;
+
+            case State.End:
+                end();
+
+                break;
+
+
+
+        }
+    }
+
+
+    private void firstcheck()
+    {
+        chara[(int)Chara.Villager] = selectorscript.VL();
+        chara[(int)Chara.Teller] = selectorscript.TE();
+        chara[(int)Chara.Medium] = selectorscript.ME();
+        chara[(int)Chara.Werewolf] = selectorscript.WE();
+        chara[(int)Chara.Madman] = selectorscript.MA();
+
+
+        for (int i = 0; i < selectorscript.PlayerNUM(); i++)
+        {
+
+            int who;
+
+            who = DecideRandom();
+
+            while (chara[who] == 0)
+            {
+
+                who = DecideRandom();
+
+            }
+
+            chara[who]--;
+
+            //インスタンス作成処理
+
+
+
+        }
+
+        game = State.Night;
+
+
+    }
+
+    private int DecideRandom()
+    {
+        int n;
+        n = Random.Range(1, 5);
+
+
+        return n;
+
+    }
+
+    private void night()
+    {
+
+    }
+
+    private void morning()
+    {
+
+    }
+
+    private void daycheck()
+    {
+
+    }
+
+    private void end()
+    {
+
+    }
+
+}
+
---
+++ Assets/Script/Selector.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Selector : MonoBehaviour
+{
+    #region オブジェクト
+    public Dropdown PlayerNum;//プレイヤー人数
+
+    public Dropdown Villager;//村人
+    public Dropdown Teller;//占い師
+    public Dropdown Medium;//霊媒師
+    public Dropdown Werewolf;//人狼
+    public Dropdown Madman;//狂人
+
+    public GameObject SelectPanel;
+
+    #endregion
+
+    #region 変数
+
+    private bool SettingComplete = false;
+
+    private int PlayerALLNum = 0;
+    private int PersonNum = 0;
+
+        private int Vl;
+        private int Te;
+        private int Me;
+        private int We;
+        private int Ma;
+
+
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SettingComplete) return;
+        PlayerALLNum = PlayerNum.value + 3;
+
+        PersonNum = Villager.value + Teller.value + Medium.value + Madman.value;
+
+        Vl = Villager.value;
+        Te = Teller.value;
+        Me = Medium.value;
+        We = Werewolf.value;
+        Ma = Madman.value;
+
+
+    }
+
+    public void GameStart()
+    {
+
+        if (PersonNum <= Werewolf.value) return;
+
+        if (Werewolf.value == 0) return;
+
+        if (PlayerALLNum != PersonNum + Werewolf.value) return;
+
+
+
+        SelectPanel.SetActive(false);
+        SettingComplete = true;
+    }
+
+    public bool GameState()
+    {
+        return SettingComplete;
+    }
+
+    public int VL()
+    {
+        return Vl; 
+    }
+
+    public int TE()
+    {
+        return Te;
+    }
+
+    public int ME()
+    {
+        return Me;
+    }
+
+    public int WE()
+    {
+        return We;
+    }
+
+    public int MA()
+    {
+        return Ma;
+    }
+
+    public int PlayerNUM()
+    {
+        return PlayerALLNum;
+
+    }
+
+
+
+}
+
